69e65ef86d39741022be785fd3ca69a4f6164ef3
ZJY <764763903@qq.com>
2022-04-06 23:37:20 +0800

    提示

--- models/log.go
+++ models/log.go
@@ -11,31 +11,31 @@ const (
 
 var flag = false
 var conn = getConn()
-
-func getConn() net.Conn {
-	if flag {
-		conn, err := net.Dial("tcp", addr)
-		if err != nil {
-			fmt.Println("连接服务端失败:", err.Error())
-			return nil
-		}
-		fmt.Println("已连接服务器")
-		flag = true
-		return conn
-	}
-
-	return conn
-}
-
-func GetLog() string {
-	n := getConn()
-	n.Write([]byte("getLog"))
-	buf := make([]byte, 1024)
-	c, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println("读取服务器数据异常:", err.Error())
-	}
-	fmt.Println(string(buf[0:c]))
-	conn.Close()
-	return string(buf[0:c])
-}
+//
+//func getConn() net.Conn {
+//	if flag {
+//		conn, err := net.Dial("tcp", addr)
+//		if err != nil {
+//			fmt.Println("连接服务端失败:", err.Error())
+//			return nil
+//		}
+//		fmt.Println("已连接服务器")
+//		flag = true
+//		return conn
+//	}
+//
+//	return conn
+//}
+//
+//func GetLog() string {
+//	n := getConn()
+//	n.Write([]byte("getLog"))
+//	buf := make([]byte, 1024)
+//	c, err := conn.Read(buf)
+//	if err != nil {
+//		fmt.Println("读取服务器数据异常:", err.Error())
+//	}
+//	fmt.Println(string(buf[0:c]))
+//	conn.Close()
+//	return string(buf[0:c])
+//}
