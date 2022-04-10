92de2766a635632775d56326de54f150ec8922c5
ZJY <764763903@qq.com>
2022-04-10 11:43:52 +0800

    内置

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
 	"github.com/beego/beego/v2/client/httplib"
 	"github.com/beego/beego/v2/core/logs"
 	"github.com/beego/beego/v2/server/web"
@@ -9,6 +11,13 @@ import (
 	"github.com/cdle/xdd/controllers"
 	"github.com/cdle/xdd/models"
 	"github.com/cdle/xdd/qbot"
+	"github.com/golang/freetype"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/image/font"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -19,7 +28,7 @@ var theme = ""
 
 var (
 	dpi      = flag.Float64("dpi", 72, "screen resolution in Dots Per Inch")
-	fontfile = flag.String("fontfile", "/simsunb.ttf", "filename of the ttf font")
+	fontfile = flag.String("fontfile", "simsunb.ttf", "filename of the ttf font")
 	hinting  = flag.String("hinting", "none", "none | full")
 	size     = flag.Float64("size", 12, "font size in points")
 	spacing  = flag.Float64("spacing", 1.5, "line spacing (e.g. 2 means double spaced)")
@@ -63,83 +72,7 @@ var text = []string{
 	"And the mome raths outgrabe.",
 }
 
-//func main() {
-//	flag.Parse()
-//
-//	// Read the font data.
-//	fontBytes, err := ioutil.ReadFile(*fontfile)
-//	if err != nil {
-//		log.Println(err)
-//		return
-//	}
-//	f, err := freetype.ParseFont(fontBytes)
-//	if err != nil {
-//		log.Println(err)
-//		return
-//	}
-//
-//	// Initialize the context.
-//	fg, bg := image.Black, image.White
-//	ruler := color.RGBA{0xdd, 0xdd, 0xdd, 0xff}
-//	if *wonb {
-//		fg, bg = image.White, image.Black
-//		ruler = color.RGBA{0x22, 0x22, 0x22, 0xff}
-//	}
-//	rgba := image.NewRGBA(image.Rect(0, 0, 640, 480))
-//	draw.Draw(rgba, rgba.Bounds(), bg, image.ZP, draw.Src)
-//	c := freetype.NewContext()
-//	c.SetDPI(*dpi)
-//	c.SetFont(f)
-//	c.SetFontSize(*size)
-//	c.SetClip(rgba.Bounds())
-//	c.SetDst(rgba)
-//	c.SetSrc(fg)
-//	switch *hinting {
-//	default:
-//		c.SetHinting(font.HintingNone)
-//	case "full":
-//		c.SetHinting(font.HintingFull)
-//	}
-//
-//	// Draw the guidelines.
-//	for i := 0; i < 200; i++ {
-//		rgba.Set(10, 10+i, ruler)
-//		rgba.Set(10+i, 10, ruler)
-//	}
-//
-//	// Draw the text.
-//	pt := freetype.Pt(10, 10+int(c.PointToFixed(*size)>>6))
-//	for _, s := range text {
-//		_, err = c.DrawString(s, pt)
-//		if err != nil {
-//			log.Println(err)
-//			return
-//		}
-//		pt.Y += c.PointToFixed(*size * *spacing)
-//	}
-//
-//	Save that RGBA image to disk.
-//	outFile, err := os.Create("D:\\work\\xdd-plus\\aaa.png")
-//	if err != nil {
-//		log.Println(err)
-//		os.Exit(1)
-//	}
-//	defer outFile.Close()
-//	b := bufio.NewWriter(outFile)
-//	err = png.Encode(b, rgba)
-//	if err != nil {
-//		log.Println(err)
-//		os.Exit(1)
-//	}
-//	err = b.Flush()
-//	if err != nil {
-//		log.Println(err)
-//		os.Exit(1)
-//	}
-//	fmt.Println("Wrote out.png OK.")
-//}
-
-func main1() {
+func main() {
 	go func() {
 		models.Save <- &models.JdCookie{}
 	}()
@@ -212,4 +145,77 @@ func main1() {
 		go qbot.Main()
 	}
 	web.Run()
+
+	flag.Parse()
+
+	// Read the font data.
+	fontBytes, err := ioutil.ReadFile(*fontfile)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	f, err := freetype.ParseFont(fontBytes)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Initialize the context.
+	fg, bg := image.Black, image.White
+	ruler := color.RGBA{0xdd, 0xdd, 0xdd, 0xff}
+	if *wonb {
+		fg, bg = image.White, image.Black
+		ruler = color.RGBA{0x22, 0x22, 0x22, 0xff}
+	}
+	rgba := image.NewRGBA(image.Rect(0, 0, 640, 480))
+	draw.Draw(rgba, rgba.Bounds(), bg, image.ZP, draw.Src)
+	c := freetype.NewContext()
+	c.SetDPI(*dpi)
+	c.SetFont(f)
+	c.SetFontSize(*size)
+	c.SetClip(rgba.Bounds())
+	c.SetDst(rgba)
+	c.SetSrc(fg)
+	switch *hinting {
+	default:
+		c.SetHinting(font.HintingNone)
+	case "full":
+		c.SetHinting(font.HintingFull)
+	}
+
+	// Draw the guidelines.
+	for i := 0; i < 200; i++ {
+		rgba.Set(10, 10+i, ruler)
+		rgba.Set(10+i, 10, ruler)
+	}
+
+	// Draw the text.
+	pt := freetype.Pt(10, 10+int(c.PointToFixed(*size)>>6))
+	for _, s := range text {
+		_, err = c.DrawString(s, pt)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		pt.Y += c.PointToFixed(*size * *spacing)
+	}
+
+	outFile, err := os.Create("aaa.png")
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer outFile.Close()
+	b := bufio.NewWriter(outFile)
+	err = png.Encode(b, rgba)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	err = b.Flush()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("Wrote out.png OK.")
 }
