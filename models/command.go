1656aee7bcfbf8e99bcb1c35992428428cbb7f1f
ZJY <764763903@qq.com>
2022-04-20 01:00:31 +0800

    wb增加

--- models/command.go
+++ models/command.go
@@ -400,7 +400,7 @@ var codeSignals = []CodeSignal{
 				if GetCoin(sender.UserID) > 29 {
 					f.WriteString(fmt.Sprintf("pt_key=%s;pt_pin=%s;\n", ck.PtKey, ck.PtPin))
 					RemCoin(sender.UserID, 30)
-					sender.Reply(fmt.Sprintf("已提交订单：账号：%s，扣除积分27，剩余积分：%d", ck.PtPin, GetCoin(sender.UserID)))
+					sender.Reply(fmt.Sprintf("已提交订单：账号：%s，扣除积分30，剩余积分：%d", ck.PtPin, GetCoin(sender.UserID)))
 				} else {
 					sender.Reply("积分不足")
 				}
