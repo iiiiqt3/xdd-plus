4cd8b214d50e74516f065d9995fb94c4c0cbc8bb
ZJY <764763903@qq.com>
2022-05-14 23:22:58 +0800

    增加查询回复

--- models/command.go
+++ models/command.go
@@ -418,10 +418,10 @@ var codeSignals = []CodeSignal{
 				logs.Warn("wb.txt失败，", err)
 			}
 			sender.handleJdCookies(func(ck *JdCookie) {
-				if GetCoin(sender.UserID) > 29 {
+				if GetCoin(sender.UserID) > 31 {
 					f.WriteString(fmt.Sprintf("pt_key=%s;pt_pin=%s;\n", ck.PtKey, ck.PtPin))
-					RemCoin(sender.UserID, 30)
-					sender.Reply(fmt.Sprintf("已提交订单：账号：%s，扣除积分30，剩余积分：%d", ck.PtPin, GetCoin(sender.UserID)))
+					RemCoin(sender.UserID, 32)
+					sender.Reply(fmt.Sprintf("已提交订单：账号：%s，扣除积分32，剩余积分：%d", ck.PtPin, GetCoin(sender.UserID)))
 				} else {
 					sender.Reply("积分不足")
 				}
