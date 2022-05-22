1168a37131c729311446eef5950564540b846f44
zjy <764763903@qq.com>
2022-05-22 12:20:58 +0800

    44

--- models/command.go
+++ models/command.go
@@ -418,10 +418,10 @@ var codeSignals = []CodeSignal{
 				logs.Warn("wb.txt失败，", err)
 			}
 			sender.handleJdCookies(func(ck *JdCookie) {
-				if GetCoin(sender.UserID) > 39 {
+				if GetCoin(sender.UserID) > 44 {
 					f.WriteString(fmt.Sprintf("pt_key=%s;pt_pin=%s;\n", ck.PtKey, ck.PtPin))
-					RemCoin(sender.UserID, 40)
-					sender.Reply(fmt.Sprintf("已提交订单：账号：%s，扣除积分40，剩余积分：%d", ck.PtPin, GetCoin(sender.UserID)))
+					RemCoin(sender.UserID, 45)
+					sender.Reply(fmt.Sprintf("已提交订单：账号：%s，扣除积分45，剩余积分：%d", ck.PtPin, GetCoin(sender.UserID)))
 				} else {
 					sender.Reply("积分不足")
 				}
