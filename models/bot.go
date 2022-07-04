2731d739579f49ec7c2959a562d0d0e9541b88d3
zjy <764763903@qq.com>
2022-07-04 10:21:18 +0800

    修复推一推

--- models/bot.go
+++ models/bot.go
@@ -1157,7 +1157,7 @@ func starttyt(red string) (num int, f bool) {
 		time.Sleep(time.Second * 10)
 		logs.Info(ck.PtPin)
 		cookie := "pt_key=" + ck.PtKey + ";pt_pin=" + ck.PtPin + ";"
-		sprintf := fmt.Sprintf(`https://api.m.jd.com/?functionId=helpCoinDozer&appid=station-soa-h5&client=H5&clientVersion=1.0.0&t=1641900500241&body={"actId":"49f40d2f40b3470e8d6c39aa4866c7ff","channel":"coin_dozer","referer":"-1","frontendInitStatus":"s","packetId":"%s","helperStatus":"0"}&_ste=1`, red)
+		sprintf := fmt.Sprintf(`https://api.m.jd.com/?functionId=helpCoinDozer&appid=station-soa-h5&client=H5&clientVersion=1.0.0&t=1641900500241&body={"actId":"3075b6eab065464dad1c4042d345ac97","channel":"coin_dozer","referer":"-1","frontendInitStatus":"s","packetId":"%s","helperStatus":"0"}&_ste=1`, red)
 		req := httplib.Post(sprintf)
 		random := browser.Random()
 		req.Header("User-Agent", random)
