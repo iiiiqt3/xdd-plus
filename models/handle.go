4d86b0394d2ad3e9cde6d72ec0f3ef5491481ac5
zjy <764763903@qq.com>
2022-07-07 11:51:12 +0800

    修复推一推

--- models/handle.go
+++ models/handle.go
@@ -112,7 +112,7 @@ module.exports = cookies`, cookies))
 							if Config.Containers[i].Mode != Parallel {
 								(&Config.Containers[i]).write(Config.Containers[i].cks)
 							} else {
-								(&Config.Containers[i]).write(cks[1:800])
+								(&Config.Containers[i]).write(cks[0:800])
 							}
 						}
 					}
