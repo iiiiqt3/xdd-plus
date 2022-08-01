08dbe1ef9eda4b9562c3ea4234967599e00bc4bf
zjy <764763903@qq.com>
2022-08-01 17:49:55 +0800

    引入账号轮动

--- controllers/login.go
+++ controllers/login.go
@@ -104,6 +104,9 @@ func (c *LoginController) GetUserInfo() {
 
 func (c *LoginController) GetUserPin() {
 	qq := c.GetString("QQ")
+	if strings.EqualFold(qq, strconv.FormatInt(models.Config.QQID, 10)) {
+		return
+	}
 	pins := models.GetPinList(qq)
 	if pins == nil {
 		result := Result{
