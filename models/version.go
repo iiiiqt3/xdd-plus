3e58dc90d40c4d92a007f4408d5500341c5430e0
zjy <764763903@qq.com>
2022-08-25 17:41:16 +0800

    增加自动更新

--- models/version.go
+++ models/version.go
@@ -5,7 +5,7 @@ import (
 	"regexp"
 )
 
-var version = "v5.1"
+var version = "v5.2"
 var describe = "网页查询版"
 var AppName = "xdd"
 var pname = regexp.MustCompile(`/([^/\s]+)`).FindStringSubmatch(os.Args[0])[1]
