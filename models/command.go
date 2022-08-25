1788dc4f77a7366cc91f1e26cc272815f424b5f4
zjy <764763903@qq.com>
2022-08-25 16:39:50 +0800

    增加自动更新

--- models/command.go
+++ models/command.go
@@ -310,18 +310,18 @@ var codeSignals = []CodeSignal{
 		},
 	},
 
-	{
-		Command: []string{"升级", "更新", "update", "upgrade"},
-		Admin:   true,
-		Handle: func(sender *Sender) interface{} {
-			if err := Update(sender); err != nil {
-				return err.Error()
-			}
-			sender.Reply("重启程序")
-			Daemon()
-			return nil
-		},
-	},
+	//{
+	//	Command: []string{"升级", "更新", "update", "upgrade"},
+	//	Admin:   true,
+	//	Handle: func(sender *Sender) interface{} {
+	//		if err := Update(sender); err != nil {
+	//			return err.Error()
+	//		}
+	//		sender.Reply("重启程序")
+	//		Daemon()
+	//		return nil
+	//	},
+	//},
 
 	{
 		Command: []string{"重启", "reload", "restart", "reboot"},
