31f2fde74fbe09e5f56ef34dcb78d27ba76459b3
zjy <764763903@qq.com>
2022-09-23 16:48:13 +0800

    剔除无用的代码

--- models/bot.go
+++ models/bot.go
@@ -55,7 +55,9 @@ type ViVoRes struct {
 }
 
 var ListenQQPrivateMessage = func(uid int64, msg string) {
-	SendQQ(uid, handleMessage(msg, "qq", int(uid)))
+	if strings.Contains(msg, "绑定微信") {
+		SendQQ(uid, handleMessage(msg, "qq", int(uid)))
+	}
 }
 
 var ListenQQTempPrivateMessage = func(uid int64, msg string) {
