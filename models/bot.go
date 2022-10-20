13bbe7784d4debfbe3f9945b58777d0eb518e8e1
zjy <764763903@qq.com>
2022-10-20 21:05:49 +0800

    剔除无用的代码

--- models/bot.go
+++ models/bot.go
@@ -181,6 +181,7 @@ var handleMessage = func(msgs ...interface{}) interface{} {
 							return "口令转换失败"
 						} else {
 							if strings.Contains(string(body), "shareType=expandHelp") {
+								sender.Reply("开始助力")
 								inviterCode := regexp.MustCompile(`inviteId=(\S+)(&|&amp;)mpin`).FindStringSubmatch(string(body))
 								k, flag := startpz(inviterCode[1])
 								if flag {
