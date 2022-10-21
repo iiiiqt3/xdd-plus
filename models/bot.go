43eecfec0018803600899859f22a65faae679587
zjy <764763903@qq.com>
2022-10-21 20:25:08 +0800

    增加金币助力，膨胀助力，组队助力，修复微信回复问题。

--- models/bot.go
+++ models/bot.go
@@ -194,7 +194,10 @@ var handleMessage = func(msgs ...interface{}) interface{} {
 								tytno += 1
 								pzlist[inviterCode[1]] = no
 								sender.Reply("开始膨胀，管理员")
-								go runpz(sender, inviterCode[1])
+								runTask(&Task{Path: "jd_tyt.js", Envs: []Env{
+									{Name: "jd_racxj_inviteIdArr_expand", Value: inviterCode[1]},{Name: "gua_racxj_token",Value: "71u0clooyrj5yyr8"},
+								}}, sender)
+								//go runpz(sender, inviterCode[1])
 							}
 						}
 					}
