4faa5100fb531ecda3532c32ae1b7e5dd75d5ddf
zjy <764763903@qq.com>
2022-11-18 15:11:31 +0800

    修复时间问题 删除错误日志

--- main.go
+++ main.go
@@ -146,15 +146,5 @@ func main() {
 		logs.Info("不启动QQ")
 	}
 
-	config := make(map[string]interface{})
-	config["filename"] = models.ExecPath + "/logs/logcollect.log"
-	config["level"] = logs.Info
-
-	configStr, err := json.Marshal(config)
-	if err != nil {
-		fmt.Println("marshal failed,err:", err)
-		return
-	}
-	logs.SetLogger(logs.AdapterFile, string(configStr))
 	web.Run()
 }
