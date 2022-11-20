09c9bda698ad1516f9ed21c74958ff9d58eee2b1
zjy <764763903@qq.com>
2022-11-20 19:27:26 +0800

    修复tg查询

--- models/command.go
+++ models/command.go
@@ -94,10 +94,10 @@ func SendWxMsg(uid string, msg string) {
 		} `json:"data"`
 	}
 
-	logs.Info(Config.Wx.Url)
+	logs.Info(Config.Wx.Url + "/DaenWxHook/httpapi/?wxid=" + Config.Wx.Robotid)
 	logs.Info(Config.Wx.Robotid)
 
-	req := httplib.Post(Config.Wx.Url+"/DaenWxHook/httpapi/?wxid="+Config.Wx.Robotid)
+	req := httplib.Post(Config.Wx.Url + "/DaenWxHook/httpapi/?wxid=" + Config.Wx.Robotid)
 	reply := &QXMessage{
 		Type: "Q0001",
 		Data: struct {
