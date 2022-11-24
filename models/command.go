3360088cf7435c217f7258a78cc935b2ab6b414a
zjy <764763903@qq.com>
2022-11-24 17:51:44 +0800

    修改登录方式

--- models/command.go
+++ models/command.go
@@ -622,7 +622,7 @@ var codeSignals = []CodeSignal{
 			u := uuid.New()
 			s := u.String()
 			SaveCache("AdminToken", s)
-			return "登录授权码为：" + s
+			return "你的授权地址为:http://xadmin.smxy.xyz/?token=" + s
 		},
 	},
 
