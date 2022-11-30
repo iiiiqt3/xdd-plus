fc2c72f06855092d36e3617d996d041deafd1fa3
iiiiqt <iiiiqt3@gmail.com>
2022-11-30 13:07:20 +0000

    更新 'models/bot.go'
    这个转码没用了

--- models/bot.go
+++ models/bot.go
@@ -2,12 +2,12 @@ package models
 
 import (
 	"crypto/md5"
-	"encoding/base64"
+//	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	browser "github.com/EDDYCJY/fake-useragent"
 	"github.com/buger/jsonparser"
-	"github.com/skip2/go-qrcode"
+//	"github.com/skip2/go-qrcode"
 	"io"
 	"io/ioutil"
 	"math/rand"
@@ -169,28 +169,28 @@ var handleMessage = func(msgs ...interface{}) interface{} {
 			}
 
 			//转码
-			{
-				if strings.Contains(msg, "https://kpl.m.jd.com/product") {
-					ss := regexp.MustCompile(`wareId=(\S+)(&|&amp;)utm_source`).FindStringSubmatch(msg)
-					url := fmt.Sprintf("https://wqdeal.jd.com/deal/confirmorder/main?commlist=%s,,1,%s,1,0,0", ss[1], ss[1])
-					data, _ := qrcode.Encode(url, qrcode.Medium, 256)
-					err2 := ioutil.WriteFile("./output.jpg", data, 0666)
-					if err2 != nil {
-						logs.Error(err2)
-					}
-					return "data:image/png;base64," + base64.StdEncoding.EncodeToString(data)
-				} else if strings.Contains(msg, "item.m.jd.com/product") {
-					var s = msg[strings.Index(msg, "product/")+8 : strings.Index(msg, ".html?")]
-					logs.Info(s)
-					url := fmt.Sprintf("https://wqdeal.jd.com/deal/confirmorder/main?commlist=%s,,1,%s,1,0,0", s, s)
-					data, _ := qrcode.Encode(url, qrcode.Medium, 256)
-					err2 := ioutil.WriteFile("./output.jpg", data, 0666)
-					if err2 != nil {
-						logs.Error(err2)
-					}
-					return "data:image/png;base64," + base64.StdEncoding.EncodeToString(data)
-				}
-			}
+//			{
+//				if strings.Contains(msg, "https://kpl.m.jd.com/product") {
+//					ss := regexp.MustCompile(`wareId=(\S+)(&|&amp;)utm_source`).FindStringSubmatch(msg)
+//					url := fmt.Sprintf("https://wqdeal.jd.com/deal/confirmorder/main?commlist=%s,,1,%s,1,0,0", ss[1], ss[1])
+//					data, _ := qrcode.Encode(url, qrcode.Medium, 256)
+//					err2 := ioutil.WriteFile("./output.jpg", data, 0666)
+//					if err2 != nil {
+//						logs.Error(err2)
+//					}
+//					return "data:image/png;base64," + base64.StdEncoding.EncodeToString(data)
+//				} else if strings.Contains(msg, "item.m.jd.com/product") {
+//					var s = msg[strings.Index(msg, "product/")+8 : strings.Index(msg, ".html?")]
+//					logs.Info(s)
+//					url := fmt.Sprintf("https://wqdeal.jd.com/deal/confirmorder/main?commlist=%s,,1,%s,1,0,0", s, s)
+//					data, _ := qrcode.Encode(url, qrcode.Medium, 256)
+//					err2 := ioutil.WriteFile("./output.jpg", data, 0666)
+//					if err2 != nil {
+//						logs.Error(err2)
+//					}
+//					return "data:image/png;base64," + base64.StdEncoding.EncodeToString(data)
+//				}
+//			}
 
 			//口令
 			{
