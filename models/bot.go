3d4b12d84ac573f6cfb009fc5fc05427d3bce3ca
zjy <764763903@qq.com>
2022-12-03 15:20:01 +0800

    V7.2 去除无用mod

--- models/bot.go
+++ models/bot.go
@@ -535,8 +535,6 @@ var handleMessage = func(msgs ...interface{}) interface{} {
 										sender.Reply("新设备登录需要验证，前往京东APP-我的-设置-账户与安全-新设备登录确认中确认，好了对我说:000000")
 										riskcodes[sender.UserID] = "true"
 									}
-									//验证
-									sender.Reply("你的账号需要验证才能登陆，请输入你的京东账号绑定的身份证前两位和后四位，最后一位如果是X，请输入大写X\n例如：31122X")
 									//做个标记
 									riskcodes[sender.UserID] = "true"
 									if arkRes.Message != "" {
