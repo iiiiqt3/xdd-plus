d075a4d4c72c7e7072c7cdb92d5d18193cbbd72b
zjy <764763903@qq.com>
2022-12-05 17:45:27 +0800

    V7.3 精确匹配规则

--- models/bot.go
+++ models/bot.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	browser "github.com/EDDYCJY/fake-useragent"
 	"github.com/buger/jsonparser"
-	"github.com/tidwall/match"
 	"github.com/tinyhubs/tinydom"
 	"io"
 	"io/ioutil"
@@ -174,8 +173,8 @@ var handleMessage = func(msgs ...interface{}) interface{} {
 
 			//返利识别
 			{
-				JdItemPattern := "^https://item(.m)?.jd.com/(product/)?([0-9]+).html"
-				b2 := match.Match(msg, JdItemPattern) || strings.Contains(msg, "https://u.jd.com/")
+				matched, _ := regexp.MatchString("^https://item(.m)?.jd.com/(product/)?([0-9]+).html", msg)
+				b2 := matched || strings.Contains(msg, "https://u.jd.com/")
 				if b2 {
 					return Get_powerful_link(msg)
 				}
