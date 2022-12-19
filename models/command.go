bad6012906c9f1ef431d3047e21d5a0891de56a6
zjy <764763903@qq.com>
2022-12-20 01:17:00 +0800

    v7.4 增加系统设置网页

--- models/command.go
+++ models/command.go
@@ -281,8 +281,8 @@ var codeSignals = []CodeSignal{
 							sender.Reply("已助力完成，正在自动领取.")
 							getmoney(cookie)
 							bytes := getShare(cookie)
-							money, _ := jsonparser.GetFloat(bytes, "data", "canUseCoinMoney")
-							sender.Reply(fmt.Sprintf("已领取完成，积分为:%f.", money))
+							money, _ := jsonparser.GetString(bytes, "data", "canUseCoinMoney")
+							sender.Reply(fmt.Sprintf("已领取完成，积分为:%s.", money))
 						}
 					} else {
 						sender.Reply("积分不足")
