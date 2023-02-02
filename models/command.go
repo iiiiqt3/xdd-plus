9e41a9d213bc133d2437ad05458fc8b12a607b58
zjy <764763903@qq.com>
2023-02-02 17:37:10 +0800

    测试

--- models/command.go
+++ models/command.go
@@ -92,8 +92,10 @@ func SendWxImg(uid string, file []byte) {
 			Path string `json:"path"`
 		} `json:"data"`
 	}
+	unix := time.Now().Unix()
 
-	filename := ExecPath + fmt.Sprintf("/%d.jpg", time.Now().Unix())
+	filename := ExecPath + fmt.Sprintf("/static/%d.jpg", unix)
+	url := fmt.Sprintf("http://192.168.195.52:5703/static/%d.jpg", unix)
 
 	f, err := os.OpenFile(ExecPath+filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0777)
 	if err != nil {
@@ -110,7 +112,7 @@ func SendWxImg(uid string, file []byte) {
 			Path string `json:"path"`
 		}{
 			Wxid: uid,
-			Path: filename,
+			Path: url,
 		},
 	}
 	random := browser.Random()
