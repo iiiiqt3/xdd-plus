072df475fe7ba5ea1857fc7eccd9132b3794f946
zjy <764763903@qq.com>
2023-02-02 16:25:26 +0800

    测试

--- models/command.go
+++ models/command.go
@@ -924,6 +924,8 @@ func getQrStatus(cookie []*http.Cookie, sender *Sender) {
 
 	for {
 		get := httplib.Get(fmt.Sprintf("http://192.168.195.53:2081/d/status?t=%d", time.Now().Unix())).SetTimeout(time.Duration(5)*time.Second, time.Duration(5)*time.Second).SetCookie(cookie[0])
+		s := cookie[0].String()
+		logs.Info(s)
 		bytes, _ := get.Bytes()
 		code, _ := jsonparser.GetInt(bytes, "code")
 		errorMsg, _ := jsonparser.GetString(bytes, "errorMsg")
