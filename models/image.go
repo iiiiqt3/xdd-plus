8204235d479f2e48c357d217919130ab8e3e7058
zjy <764763903@qq.com>
2023-02-14 09:25:43 +0800

    测试

--- models/image.go
+++ models/image.go
@@ -110,7 +110,7 @@ func uploadImg(filename string) string {
 	get := httplib.Post("http://images.smxy.xyz/api/v1/upload")
 	get.Header("Authorization", "3|vYO0BddOAoHfIJnZEqNG11OmxLzdC5kASfP1JUFi")
 	//get.Header("Content-Type", "multipart/form-data")
-	get.PostFile("file", filename)
+	//get.PostFile("file", filename)
 	bytes, _ := get.Bytes()
 	logs.Info(string(bytes))
 	s, _ := jsonparser.GetString(bytes, "data", "links", "url")
