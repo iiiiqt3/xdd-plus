21d9d8cba5bcbc68c69c18e0c302e4dba191fb0b
zjy <764763903@qq.com>
2023-02-23 17:43:56 +0800

    新增rabbit API

--- models/rabbit.go
+++ models/rabbit.go
@@ -10,6 +10,7 @@ import (
 	"gorm.io/gorm"
 	"math/rand"
 	"net/url"
+	"strconv"
 	"time"
 )
 
@@ -36,8 +37,10 @@ func getJDQrStatus(cookie string, sender *Sender) {
 		get := httplib.Post(fmt.Sprintf("http://192.168.195.53:5800/api/QrCheck?token=%s", "sad5d5s6c5d5e8w6r6t6uiopfghf5s6ew5ds8c12b"))
 		marshal, _ := json.Marshal(struct {
 			QRCodeKey string `json:"QRCodeKey"`
+			Qlkey     string `json:"qlkey"`
 		}{
 			QRCodeKey: cookie,
+			Qlkey:     strconv.Itoa(0),
 		},
 		)
 		get.Body(marshal)
