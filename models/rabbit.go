4e87e2e873ecc762a9e73083c824527875704772
zjy <764763903@qq.com>
2023-02-23 17:55:39 +0800

    新增rabbit API

--- models/rabbit.go
+++ models/rabbit.go
@@ -125,7 +125,8 @@ func UpdateRwskey() {
 
 		time.Sleep(time.Duration(rand.Int63n(1)) * time.Second)
 		//JdCookie{}.Push(fmt.Sprintf("更新账号账号，%s", ck.Nickname))
-		var pinky = fmt.Sprintf("pin=%s;wskey=%s;", ck.PtPin, ck.RWskey)
+		pin, _ := url.QueryUnescape(ck.PtPin)
+		var pinky = fmt.Sprintf("pin=%s;wskey=%s;", pin, ck.RWskey)
 		rsp, _, appck := GetCookie(pinky)
 		if rsp {
 			ptKey := FetchJdCookieValue("pt_key", appck)
