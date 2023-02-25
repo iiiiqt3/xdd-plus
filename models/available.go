594a20b15c2a3a4a55d49ead859db45cf6da98d1
zjy <764763903@qq.com>
2023-02-25 11:44:06 +0800

    禁用QQpush，启用微信Push，Wskey失效置空

--- models/available.go
+++ models/available.go
@@ -235,8 +235,9 @@ func updateCookie() {
 			rsp, _ := getKey(pinky)
 			if strings.Contains(rsp, "错误") {
 				yy++
-				ck.Update(Available, False)
-				ck.Update(WsKey, "")
+				ck.Updates(JdCookie{Available: False, WsKey: ""})
+				//ck.Update(Available, False)
+				//ck.Update(WsKey, "")
 				ck.Push(fmt.Sprintf("年费Wskey失效账号，%s，请联系管理员", ck.PtPin))
 				(&JdCookie{}).Push(fmt.Sprintf("年费Wskey失效，%s", ck.PtPin))
 			} else {
