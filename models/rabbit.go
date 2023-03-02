787ede96eff514329346ba7875dd6578e5409a47
zjy <764763903@qq.com>
2023-03-02 10:38:44 +0800

    修改Wskey中文昵称转换问题

--- models/rabbit.go
+++ models/rabbit.go
@@ -139,7 +139,7 @@ func UpdateRwskey() {
 			} else {
 				yy++
 				logs.Info(appck)
-				(&JdCookie{}).Push(fmt.Sprintf("转换失败，请求超时，账号:%s", ck.PtPin))
+				(&JdCookie{}).Push(fmt.Sprintf("Wskey失效，账号:%s", ck.PtPin))
 			}
 
 		} else {
