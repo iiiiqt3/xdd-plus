b298eb455f678f7c17ebfeb90401a044459ec6f1
zjy <764763903@qq.com>
2023-03-27 11:24:57 +0800

    升级ws连接

--- controllers/qq.go
+++ controllers/qq.go
@@ -1,15 +1,9 @@
 package controllers
 
 import (
-	"encoding/json"
 	"github.com/beego/beego/v2/core/logs"
 	"github.com/cdle/xdd/models"
-	"github.com/gorilla/websocket"
 )
-
-var upgrader = websocket.Upgrader{}
-var ws *websocket.Conn
-var mt int
 
 type QQController struct {
 	BaseController
@@ -40,46 +34,6 @@ type CqMessage struct {
 	MessageID int         `json:"message_id"`
 	Anonymous interface{} `json:"anonymous"`
 	Font      int         `json:"font"`
-}
-
-func (c *QQController) Echo() {
-	//服务升级，对于来到的http连接进行服务升级，升级到ws
-	var err error
-	ws, err = upgrader.Upgrade(c.Ctx.ResponseWriter, c.Ctx.Request, nil)
-	//defer ws.Close()
-	if err != nil {
-		panic(err)
-	}
-	for {
-		//messageType int, p []byte, err error
-		nt, message, err := ws.ReadMessage()
-		mt = nt
-		if err != nil {
-			logs.Info("read:", err)
-			break
-		}
-		logs.Info("recv: %s , %d", message, mt)
-		var msg CqMessage
-		err = json.Unmarshal(message, &msg)
-		if err != nil {
-			logs.Error(err)
-			break
-		}
-		HandleQQMessage(msg)
-
-		//err = cn.WriteMessage(mt, message)
-		//if err != nil {
-		//	logs.Info("write:", err)
-		//	break
-		//}
-	}
-}
-
-func WriteMsg(msg []byte) {
-	err := ws.WriteMessage(mt, msg)
-	if err != nil {
-		logs.Info("write:", err)
-	}
 }
 
 func HandleQQMessage(msg CqMessage) {
