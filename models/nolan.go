c869e9415492257dfbab4aa132a8db3d06b139b0
zjy <764763903@qq.com>
2023-03-29 14:49:43 +0800

    v8.9  增加消息队列优化并发消息导致的崩溃

--- models/nolan.go
+++ models/nolan.go
@@ -102,7 +102,7 @@ func NolanGetJDQrStatus(cookie string, sender *Sender) {
 			return
 		} else {
 			msg, _ := jsonparser.GetString(bytes, "message")
-			if msg != "" {
+			if msg != "二维码未扫描，请扫描二维码" && msg != "请手机客户端确认登录" {
 				sender.Reply(msg)
 				return
 			}
