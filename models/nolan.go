f95c04ad3ebd36951453ac231bc8dc3d8aa65194
zjy <764763903@qq.com>
2023-03-30 15:12:01 +0800

    v9.4 修复nolan过期回复

--- models/nolan.go
+++ models/nolan.go
@@ -108,6 +108,9 @@ func NolanGetJDQrStatus(cookie string, sender *Sender) {
 			if msg != "二维码未扫描，请扫描二维码" && msg != "请手机客户端确认登录" {
 				sender.Reply(msg)
 				return
+			} else if msg == "请先获取二维码" {
+				sender.Reply("二维码失效")
+				return
 			}
 		}
 	}
