025b7f9d983dbb02427073ac6c307bedc69f2180
zjy <764763903@qq.com>
2023-05-24 11:46:50 +0800

    v10.6  接入短信Wskey

--- models/nolan.go
+++ models/nolan.go
@@ -139,7 +139,7 @@ func NolanGetJDQrStatus(cookie string, sender *Sender) {
 }
 
 func NolanGetCookie(cookie string) (bool, string, string) {
-	get := httplib.Post(fmt.Sprintf("%s/env/wskey", NolanUrl))
+	get := httplib.Post(fmt.Sprintf("%s/env/wskey", "http://192.168.195.53:5016"))
 	get.Header("Content-Type", "application/json")
 	//get.Body(fmt.Sprintf("{\n  \"botApiToken\": \"%s\",\n  \"wskey\": \"%s\"\n}", NolanToken, cookie))
 	get.Body(fmt.Sprintf("{\n  \"botApiToken\": \"%s\",\n  \"wskey\": \"%s\"\n}", "ashdjkasdkasjdlksjsadkldase", cookie))
