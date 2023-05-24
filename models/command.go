0e5dc1a92db88473810d9692458c4cdd29f5b566
zjy <764763903@qq.com>
2023-05-24 11:32:38 +0800

    v10.6  接入短信Wskey

--- models/command.go
+++ models/command.go
@@ -1153,13 +1153,13 @@ var codeSignals = []CodeSignal{
 		Handle: func(sender *Sender) interface{} {
 			sender.handleJdCookies(func(ck *JdCookie) {
 				if len(ck.WsKey) > 0 {
-					var pinky = fmt.Sprintf("pin=%s;wskey=%s;", ck.PtPin, ck.WsKey)
+					var pinky = fmt.Sprintf("pin=%s;wskey=%s;", ck.PtPin, ck.RWskey)
 					logs.Info(pinky)
 					_, _, rsp := NolanGetCookie(pinky)
 					_, _, rsp = BBKGetCookie(pinky)
 
 					pin, _ := url.QueryUnescape(ck.PtPin)
-					pinky = fmt.Sprintf("pin=%s;wskey=%s;", pin, ck.WsKey)
+					pinky = fmt.Sprintf("pin=%s;wskey=%s;", pin, ck.RWskey)
 					_, _, rsp = RabbitGetCookie(pinky)
 
 					if len(rsp) > 0 {
