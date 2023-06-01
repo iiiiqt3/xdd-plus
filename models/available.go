9dae96c3e6b3828129f75dd2a78a1f93cf4bd1f2
zjy <764763903@qq.com>
2023-06-02 01:10:30 +0800

    v10.8 修改WSKEY更新，

--- models/available.go
+++ models/available.go
@@ -218,7 +218,7 @@ func updateCookie() {
 		if i == len(cks)/2 {
 			(&JdCookie{}).Push("Wskey已更新二分一")
 		}
-		time.Sleep(time.Duration(10) * time.Second)
+		time.Sleep(time.Duration(20) * time.Second)
 		var pinky = fmt.Sprintf("pin=%s;wskey=%s;", ck.PtPin, ck.WsKey)
 		rsp := getKey(pinky)
 		if strings.Contains(rsp, "错误") {
