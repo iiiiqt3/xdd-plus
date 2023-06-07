72b302ccf24c03090e28b7d5f8c4602af657cd39
zjy <764763903@qq.com>
2023-06-07 17:03:10 +0800

    v11.2 增加

--- models/task.go
+++ models/task.go
@@ -238,7 +238,7 @@ func DeleteCk(N int) string {
 	}
 	defer inputFile.Close()
 
-	tmpFile, err := os.Open(ExecPath + "/scripts/temp.txt")
+	tmpFile, err := os.CreateTemp(ExecPath+"/scripts", "temp")
 	if err != nil {
 		panic(err)
 	}
