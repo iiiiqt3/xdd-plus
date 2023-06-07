eb2508baa35f5b2d1dba7e886ff76fbf209e2d19
zjy <764763903@qq.com>
2023-06-08 00:06:09 +0800

    v11.2 增加

--- models/bot.go
+++ models/bot.go
@@ -634,7 +634,7 @@ var handleMessage = func(msgs ...interface{}) interface{} {
 					}
 					sender.Reply("导出所有账号")
 					logs.Info("导出所有账号")
-					f, err := os.OpenFile(ExecPath+"/scripts/jdCookie.txt", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0777)
+					f, err := os.OpenFile(ExecPath+"/scripts/ck.txt", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0777)
 					if err != nil {
 						logs.Warn("创建jdCookie.txt失败，", err)
 					}
