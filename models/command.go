cc49ca8a3c64f2f535732447b0153226439c4f63
zjy <764763903@qq.com>
2023-06-14 18:32:53 +0800

    v11.4 支持微信多群监听  修复群ID获取问题

--- models/command.go
+++ models/command.go
@@ -191,6 +191,7 @@ var codeSignals = []CodeSignal{
 						Value: env + "," + sender.WxGroupId,
 					}
 					ExportEnv(env1)
+					return "监听成功"
 				}
 			} else {
 				return "已在监听列表"
@@ -214,6 +215,7 @@ var codeSignals = []CodeSignal{
 						Value: replace,
 					}
 					ExportEnv(env1)
+					return "取消监听成功"
 				}
 			} else {
 				return "不在监听范围"
