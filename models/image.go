6dc9553d4f315446fae9160163f045a0a0bb4ff7
zjy <764763903@qq.com>
2023-07-11 18:25:31 +0800

    v11.7 修改代理地址为系统配置

--- models/image.go
+++ models/image.go
@@ -107,7 +107,7 @@ func strtoimg(str string) []byte {
 
 func uploadImg(filename string) string {
 	get := httplib.Post("http://images.smxy.xyz/api/v1/upload")
-	get.Header("Authorization", "Bearer 3|vYO0BddOAoHfIJnZEqNG11OmxLzdC5kASfP1JUFi")
+	//get.Header("Authorization", "Bearer 3|vYO0BddOAoHfIJnZEqNG11OmxLzdC5kASfP1JUFi")
 	get.Header("Content-Type", "multipart/form-data")
 	get.PostFile("file", filename)
 	bytes, _ := get.Bytes()
