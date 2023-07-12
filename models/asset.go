4f624ed225b038b1aa9caab06eabf8cd461fd206
iiiiqt <iiiiqt3@gmail.com>
2023-07-12 06:45:44 +0000

    更新 models/asset.go

--- models/asset.go
+++ models/asset.go
@@ -203,7 +203,7 @@ func (ck *JdCookie) Query() string {
 		go initFarm(cookie, fruit)
 		go jsGold(cookie, gold)
 		//		go jdzz(cookie, zjb)
-		//		go jingxiangzhi(cookie,' jxzz)
+		//		go jingxiangzhi(cookie, jxzz)
 		today := time.Now().Local().Format("2006-01-02")
 		yestoday := time.Now().Local().Add(-time.Hour * 24).Format("2006-01-02")
 		page := 1
