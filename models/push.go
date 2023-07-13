d5179e2b4fb683477d8183a2b34d119e118e07bc
zjy <764763903@qq.com>
2023-07-13 18:04:18 +0800

    v11.9 修复Wskey短信匹配

--- models/push.go
+++ models/push.go
@@ -19,6 +19,7 @@ func (ck JdCookie) Push(msg string) {
 		go SendQQ(Config.QQID, msg)
 		go qywxNotify(&QywxConfig{QywxKey: Config.QywxKey, Content: msg})
 		go SendTgMsg(Config.TelegramUserID, msg)
+
 	}
 }
 
