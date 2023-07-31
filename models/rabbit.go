4ee859c1ad43c9b788e72a9b0eb18fc1cc4ab596
zjy <764763903@qq.com>
2023-07-31 18:20:39 +0800

    v11.9 修复口令

--- models/rabbit.go
+++ models/rabbit.go
@@ -27,14 +27,21 @@ func RabbitGetJdQrImg(sender *Sender) {
 		key, _ := jsonparser.GetString(bytes, "QRCodeKey")
 
 		//返回口令
-		jcommond, _ := jsonparser.GetString(bytes, "jcommond")
-		sender.Reply(jcommond)
+		//jcommond, _ := jsonparser.GetString(bytes, "jcommond")
+		//sender.Reply(jcommond)
 
+		//图片
 		//sender.Reply(NolanLJToKL("https://qr.m.jd.com/p?k="+key, "京东快捷登录"))
-
+		//
 		//qr, _ := jsonparser.GetString(bytes, "qr")
 		//decodeStr, _ := base64.StdEncoding.DecodeString(qr)
 		//sender.SendImg(decodeStr)
+
+		//口令转
+		lj := LJtoLJ("https://qr.m.jd.com/p?k=" + key)
+		url, _ := jsonparser.GetString(lj, "code")
+		logs.Info(url)
+		sender.Reply(NolanLJToKL(url, "京东快捷登录"))
 
 		sender.Reply("请复制口令到京东APP登录，150秒失效")
 		go RabbitGetJDQrStatus(key, sender)
