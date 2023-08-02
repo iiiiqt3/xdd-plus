34b526fcc11c7b71b44034ef7cc0d5d31c0f674f
zjy <764763903@qq.com>
2023-08-02 18:17:55 +0800

    v12.1 使用图床，更新管理页面

--- models/image.go
+++ models/image.go
@@ -120,6 +120,7 @@ func uploadImg(filename string) string {
 	get := httplib.Post("http://images.smxy.xyz/api/v1/upload")
 	get.Header("Authorization", sysConfig.ImageToken)
 	get.Header("Content-Type", "multipart/form-data")
+	get.Param("strategy_id", "1")
 	get.PostFile("file", filename)
 	bytes, _ := get.Bytes()
 	logs.Info(string(bytes))
