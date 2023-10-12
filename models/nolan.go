786dcd3c362afab08be792ba48ff69ab49a771b6
zjy <764763903@qq.com>
2023-10-12 22:29:06 +0800

    v 13.1 新增短信多次验证，二次多次验证

--- models/nolan.go
+++ models/nolan.go
@@ -241,8 +241,8 @@ func NolanSendCode(phone string, code string, sender *Sender) {
 			}
 		} else if state == 404 {
 			if message == "验证码输入错误" {
-				sender.Reply("验证码输入错误,请重新登录")
-				smsList[sender.UserID] = nil
+				sender.Reply("验证码输入错误,请重新输入")
+				//smsList[sender.UserID] = nil
 			} else {
 				smsList[sender.UserID] = nil
 				sender.Reply("你的账号需要验证才能登陆，请晚上20点后再次尝试验证")
