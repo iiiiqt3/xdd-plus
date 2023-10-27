bc3ab5ed2fdace0cb5201559c07c12e9fabfdab1
zjy <764763903@qq.com>
2023-10-27 16:31:01 +0800

    v 13.5 内置微信机器人

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/beego/beego/v2/server/web/filter/cors"
 	"github.com/cdle/xdd/controllers"
 	"github.com/cdle/xdd/models"
+	"github.com/eatmoreapple/openwechat"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -199,6 +200,32 @@ func main() {
 		(&models.JdCookie{}).Push(fmt.Sprintf("小滴滴已启动，版本号:%s", models.Config.Version))
 
 	}()
+
+	bot := openwechat.DefaultBot(openwechat.Desktop) // 桌面模式
+
+	// 注册消息处理函数
+	bot.MessageHandler = func(msg *openwechat.Message) {
+		if msg.IsText() && msg.Content == "ping" {
+			msg.ReplyText("pong")
+		}
+	}
+	// 注册登陆二维码回调
+	bot.UUIDCallback = openwechat.PrintlnQrcodeUrl
+
+	// 登陆
+	reloadStorage := openwechat.NewFileHotReloadStorage("storage.json")
+	defer reloadStorage.Close()
+	err := bot.PushLogin(reloadStorage, openwechat.NewRetryLoginOption())
+
+	// 获取登陆的用户
+	self, err := bot.GetCurrentUser()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 阻塞主goroutine, 直到发生异常或者用户主动退出
+	bot.Block()
 
 	web.Run()
 
