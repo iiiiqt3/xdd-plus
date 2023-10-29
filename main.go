e3c58f068983c70adca496eebcad223057bad8c7
zjy <764763903@qq.com>
2023-10-30 00:59:08 +0800

    v 13.5 修复Rwskey转换

--- main.go
+++ main.go
@@ -202,40 +202,41 @@ func main() {
 
 	}()
 
-	bot := openwechat.DefaultBot(openwechat.Desktop) // 桌面模式
-
-	// 注册消息处理函数
-	bot.MessageHandler = func(msg *openwechat.Message) {
-		if msg.IsText() && msg.Content == "ping" {
-
-			msg.ReplyText("pong")
-
-			name := msg.FromUserName
-			id := friedns.GetByUsername(name).User
-			id.Detail()
-			//msg.ReplyText(id)
-
-		}
-	}
-	// 注册登陆二维码回调
-	bot.UUIDCallback = openwechat.PrintlnQrcodeUrl
-
-	// 登陆
-	reloadStorage := openwechat.NewFileHotReloadStorage("storage.json")
-	defer reloadStorage.Close()
-	err := bot.PushLogin(reloadStorage, openwechat.NewRetryLoginOption())
-
-	// 获取登陆的用户
-	self, err := bot.GetCurrentUser()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	friedns, err = self.Friends()
-
-	// 阻塞主goroutine, 直到发生异常或者用户主动退出
-	bot.Block()
+	//
+	//bot := openwechat.DefaultBot(openwechat.Desktop) // 桌面模式
+	//
+	//// 注册消息处理函数
+	//bot.MessageHandler = func(msg *openwechat.Message) {
+	//	if msg.IsText() && msg.Content == "ping" {
+	//
+	//		msg.ReplyText("pong")
+	//
+	//		name := msg.FromUserName
+	//		id := friedns.GetByUsername(name).User
+	//		id.Detail()
+	//		//msg.ReplyText(id)
+	//
+	//	}
+	//}
+	//// 注册登陆二维码回调
+	//bot.UUIDCallback = openwechat.PrintlnQrcodeUrl
+	//
+	//// 登陆
+	//reloadStorage := openwechat.NewFileHotReloadStorage("storage.json")
+	//defer reloadStorage.Close()
+	//err := bot.PushLogin(reloadStorage, openwechat.NewRetryLoginOption())
+	//
+	//// 获取登陆的用户
+	//self, err := bot.GetCurrentUser()
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+	//
+	//friedns, err = self.Friends()
+	//
+	//// 阻塞主goroutine, 直到发生异常或者用户主动退出
+	//bot.Block()
 
 	web.Run()
 
