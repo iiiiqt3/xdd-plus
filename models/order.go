af2d88bcc27313c1151fbbc3e173182ee8ff67b6
zjy <764763903@qq.com>
2023-12-22 18:31:44 +0800

    v13.6 完善订单系统

--- models/order.go
+++ models/order.go
@@ -29,24 +29,26 @@ func (oq *OrderQueue) ProcessOrder() *Order {
 	if len(oq.orders) == 0 {
 		return nil
 	}
-
 	order := oq.orders[0]
 	oq.orders = oq.orders[1:]
-
 	return order
 }
 
-func initOrder(activity *OrderQueue, activityName string, inviteIdName string) {
+func initOrder(activity *OrderQueue) {
 	for {
 		order := activity.ProcessOrder()
 		if order == nil {
 			time.Sleep(time.Second * time.Duration(3))
 			continue
 		}
-
 		fmt.Printf("Processing order %d: %s\n", order.id, order.name)
 		runTask(&Task{Path: order.taskName, Envs: order.envs}, order.Sender)
 		order.Sender.Reply(fmt.Sprintf("订单ID:%d已完成", order.id))
-
 	}
+}
+
+func getNewOrder() *OrderQueue {
+	var order = &OrderQueue{}
+	initOrder(order)
+	return order
 }
