4eb479dfcf6fb8060df959ea90727a1269047504
zjy <764763903@qq.com>
2024-02-23 13:57:00 +0800

    v13.8 新增美团自动领卷

--- models/cron.go
+++ models/cron.go
@@ -39,7 +39,7 @@ func initCron() {
 
 	//每天定时1点执行美团自动领卷
 	if Config.QQID == 764763903 {
-		c.AddFunc("0 0 12 * * ?", Meituan_Auto)
+		c.AddFunc("0 0 14 * * ?", Meituan_Auto)
 	}
 
 	c.AddFunc(strconv.Itoa(rand.Intn(59))+" "+strconv.Itoa(rand.Intn(24))+" * * ?", getAuthFlag)
