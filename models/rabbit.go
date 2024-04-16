0ef588b40c27600de6992f2335e62ed031967ddd
zjy <764763903@qq.com>
2024-04-16 10:09:32 +0800

    v14.1 修改登录方式

--- models/rabbit.go
+++ models/rabbit.go
@@ -30,20 +30,11 @@ func RabbitGetJdQrImg(sender *Sender) {
 
 		//返回口令
 		jcommond, _ := jsonparser.GetString(bytes, "jcommond")
-		sender.Reply(jcommond)
-
-		//图片
-		//sender.Reply(NolanLJToKL("https://qr.m.jd.com/p?k="+key, "京东快捷登录"))
+		sender.Reply(jcommond + " 请复制口令到京东APP打开")
 
 		qr, _ := jsonparser.GetString(bytes, "qr")
 		decodeStr, _ := base64.StdEncoding.DecodeString(qr)
 		sender.SendImg(decodeStr)
-
-		//口令转
-		//lj := LJtoLJ("https://qr.m.jd.com/p?k=" + key)
-		//url, _ := jsonparser.GetString(lj, "code")
-		//logs.Info(url)
-		//sender.Reply(NolanLJToKL(url, "京东快捷登录"))
 
 		sender.Reply("请使用京东APP扫描，150秒失效")
 		go RabbitGetJDQrStatus(key, sender)
