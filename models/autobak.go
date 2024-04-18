8fede933ec5100e4f6693706aaac2640f2c14577
zjy <764763903@qq.com>
2024-04-18 10:03:15 +0800

    v14.1 修改登录方式

--- models/autobak.go
+++ models/autobak.go
@@ -33,7 +33,7 @@ func AutoBak() {
 func Reduction() {
 	var cks []BakJdCookie
 	//寻找Rwskey不为空的数据
-	db.Find(&cks, "RWsKey != ?", "null")
+	db.Find(&cks, "RWskey != ?", "")
 	for _, ck := range cks {
 		if nck, err := GetJdCookie(ck.PtPin); err == nil {
 			nck.Updates(JdCookie{
