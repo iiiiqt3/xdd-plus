3829cda8e34296905ea6a9cdbf3af1509ad86ad4
zjy <764763903@qq.com>
2024-04-22 15:29:02 +0800

    v14.1 优化

--- models/bot.go
+++ models/bot.go
@@ -204,8 +204,7 @@ var handleMessage = func(msgs ...interface{}) interface{} {
 					//提取出url
 					re := regexp.MustCompile(`http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+`)
 					match := re.FindString(msg)
-					return match
-					Meituan_getUUID(msg, sender)
+					Meituan_getUUID(match, sender)
 				}
 			}
 
