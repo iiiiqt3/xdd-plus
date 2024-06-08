a834679220c695b245ca954962ef7baf7d25de5e
YH <18020203322@qq.com>
2024-06-08 14:55:30 +0000

    更新 controllers/login.go
    修复SMSLogin更新ck不进青龙bug
    
    Signed-off-by: YH <18020203322@qq.com>

--- controllers/login.go
+++ controllers/login.go
@@ -580,6 +580,9 @@ func (c *LoginController) SMSLogin() {
 					msg := fmt.Sprintf("来自短信的更新,账号：%s,QQ: %v", nck.PtPin, qq)
 					ck.Push(ck.Query())
 					(&models.JdCookie{}).Push(msg)
+					go func() {
+						models.Save <- &models.JdCookie{}
+					}()
 
 				} else {
 					models.NewJdCookie(ck)
@@ -591,6 +594,9 @@ func (c *LoginController) SMSLogin() {
 						(&models.JdCookie{}).Push(msg)
 					}
 					ck.Push(ck.Query())
+					go func() {
+						models.Save <- &models.JdCookie{}
+					}()
 
 				}
 
