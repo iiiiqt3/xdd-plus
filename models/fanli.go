f05a7155ed91c48a3e3175e3e3a9a00c5984982a
zjy <764763903@qq.com>
2024-09-26 10:43:15 +0800

    v1.0

--- models/fanli.go
+++ models/fanli.go
@@ -16,7 +16,8 @@ type FanLi struct {
 var powerful = "https://api.jingpinku.com/get_powerful_link/api"
 
 func Get_powerful_link(content string) string {
-	url := fmt.Sprintf("https://api.jingpinku.com/get_wire_report_link/api?appid=%s&appkey=%s&union_id=%s&content=%s", Config.FanLis.Appid, Config.FanLis.Appkey, Config.FanLis.Union_id, content)
+	//url := fmt.Sprintf("https://api.jingpinku.com/get_wire_report_link/api?appid=%s&appkey=%s&union_id=%s&content=%s", Config.FanLis.Appid, Config.FanLis.Appkey, Config.FanLis.Union_id, content)
+	url := fmt.Sprintf("https://api.jingpinku.com/get_wire_report_link/api?appid=%s&appkey=%s&union_id=%s&content=%s", "2204290013243761", "NwG3i8LfdTyVM557kiLpVCTjMKtU06eA", "2024487473", content)
 	logs.Informational(url)
 	get := httplib.Get(url)
 	s, _ := get.Bytes()
