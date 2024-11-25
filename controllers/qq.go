ff138abb0028b674d7f82ee835e3fd6a80150f1d
zjy <764763903@qq.com>
2024-11-25 16:21:16 +0800

    v1.0

--- controllers/qq.go
+++ controllers/qq.go
@@ -102,7 +102,7 @@ func (c *QQController) Echo() {
 }
 
 func HandleQQMessage(msg LLMessage) {
-	if msg.PostType == "message" {
+	if msg.PostType == "message_sent" {
 		logs.Info("接收到信息" + msg.RawMessage)
 		if msg.MessageType == "private" {
 			models.ListenQQPrivateMessage(msg.UserId, msg.RawMessage)
